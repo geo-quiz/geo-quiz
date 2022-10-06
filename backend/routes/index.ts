36ea4b34a2749826271549b1d5bf6142ee4a43ad
Madelene <you@example.com>
2022-10-06 14:42:27 +0200

    Add quizRouter and userRouter and json

---
+++ backend/routes/index.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+import { defaultRoute } from './defaultRoute';
+import {quizRoute} from './quiz';
+import {userRoute} from './user';
+
+export const routes = express.Router();
+
+routes.use(defaultRoute);
+routes.use(quizRoute);
+routes.use(userRoute);
