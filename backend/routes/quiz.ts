ea64535ee414aea13bc73996c0d613f068bb6982
Madelene <you@example.com>
2022-10-07 16:45:24 +0200

    Updated with get by id

--- backend/routes/quiz.ts
+++ backend/routes/quiz.ts
@@ -1,20 +1,28 @@
-import { Router } from 'express';
-import questions from '../json/questions.json';
+import express, {Application, Router} from "express";
+import questions from '../questions.json';
 
 
-
-console.log(questions.id);
-console.log(questions.question);
-console.log(questions.answers);
-console.log(questions.correctAnswer);
-console.log(questions.continent);
 const baseUrl = '/quiz';
 
+//route
 export const quizRoute = Router();
 
-//routes
 quizRoute.get(baseUrl, (_req, res) => {
     res.json(questions);
-
 });
+
+
+quizRoute.get('/quiz/:id', (req, res) => {
+    const quiz = req.params.id;
+    const quizItem = questions.questions.filter(quizId => {
+        return quizId.id.toString() === quiz;
+    });
+
+    if (quizItem.length >0){
+        res.json(quizItem);
+    } else {
+        res.json({message: `quizitem ${quiz} doesn't exist`})
+    }
+});
+
 
