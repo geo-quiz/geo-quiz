51d7daf68d29be596fa528c30e301c50415044ae
Marcus Leeman <marcus.leeman@iths.se>
2022-10-11 16:11:20 +0200

    Update HomeView to look like the home-screen

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,36 @@
 <script lang="ts" setup>
-</script>
+import GeoButton from '@/components/GeoButton.vue';</script>
 
 <template>
     <main>
-        <h1>Home</h1>
+        <GeoButton font-size="2.5rem" width="90% + 10px" height="120px" id="play-button">Spela</GeoButton>
+        <div class="buttons">
+            <GeoButton height="70px">Inställningar</GeoButton>
+            <GeoButton height="70px">Leaderboard</GeoButton>
+            <GeoButton height="70px">Profil</GeoButton>
+            <GeoButton height="70px">Logga ut</GeoButton>
+        </div>
     </main>
 </template>
+
+<style scoped>
+
+.buttons {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: space-between;
+    width: 90%;
+}
+
+main {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    gap: 20px;
+}
+
+#play-button {
+    background: #5bb318;
+}
+</style>
