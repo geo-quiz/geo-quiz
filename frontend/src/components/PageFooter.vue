a713d1754dc9a5d1e19277897c76ca2194acffe1
ChristianLofqvist <christian.lofqvist@iths.se>
2022-10-12 13:04:08 +0200

    Add component: PageFooter

---
+++ frontend/src/components/PageFooter.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <footer>
+        <p>Made by: Marcus, Toni, Oscar, Christian, Madde och Juris</p>
+        <p>&COPY; Copyright 2022</p>
+    </footer>
+</template>
+
+<style scoped>
+footer {
+    display: flex;
+    flex-wrap: wrap;
+    gap: 4px;
+    text-align: center;
+}
+
+p {
+    font-size: 0.75rem;
+    margin: 0;
+    width: 100%;
+}
+</style>
