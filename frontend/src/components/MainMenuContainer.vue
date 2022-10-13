f0d25cefa9af9d5bb71b7a50205696a7eac4d069
Marcus Leeman <marcus.leeman@iths.se>
2022-10-13 10:54:41 +0200

    Add component: MainMenuContainer

---
+++ frontend/src/components/MainMenuContainer.vue
@@ -0,0 +1,37 @@
+<script lang="ts" setup>
+import GeoLogo from '@/components/GeoLogo.vue';
+
+defineProps({
+    noLogo: {
+        type: Boolean,
+        default: false,
+    },
+});
+</script>
+
+<template>
+    <div class="background"></div>
+    <main>
+        <GeoLogo v-if="!noLogo" />
+
+        <slot></slot>
+    </main>
+</template>
+
+<style scoped>
+.background {
+    background: url('/images/background.png');
+    background-size: cover;
+    z-index: -10;
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 100px;
+}
+</style>
