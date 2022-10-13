ac7d7ea7eb7503624df72a42bc549426000bddca
Marcus Leeman <marcus.leeman@iths.se>
2022-10-13 11:50:24 +0200

    Move code from StartPage to StartView

--- frontend/src/views/StartView.vue
+++ frontend/src/views/StartView.vue
@@ -1,7 +1,25 @@
 <script lang="ts" setup>
-import StartPage from '@/components/StartPage.vue';
-</script>
+import router from '@/router/index';
+import MainMenuContainer from '@/components/MainMenuContainer.vue';
+import GeoButton from '@/components/GeoButton.vue';</script>
 
 <template>
-    <StartPage />
+    <MainMenuContainer>
+        <div class="buttonContainer">
+            <GeoButton id="login-button" height="75px" width="400px" @click="router.push('/login')">Sign in</GeoButton>
+            <GeoButton height="75px" width="400px" @click="router.push('/register')">Register</GeoButton>
+        </div>
+    </MainMenuContainer>
 </template>
+
+<style scoped>
+.buttonContainer {
+    display: flex;
+    flex-direction: column;
+    gap: var(--gap);
+}
+
+#login-button {
+    background: #5bb318;
+}
+</style>
