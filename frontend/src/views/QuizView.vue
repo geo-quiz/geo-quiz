643386439c5794fc7d6d4cf80d884ea7614caaa9
OscarStjernfeldt <oscar.eriksson.stjernfeldt@iths.se>
2022-10-13 14:24:54 +0200

    Add PageHeader to QuizView

--- frontend/src/views/QuizView.vue
+++ frontend/src/views/QuizView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 
 import QuizQuestion from '@/components/QuizQuestion.vue';
+import PageHeader from '@/components/PageHeader.vue';
 </script>
 
 <template>
+    <PageHeader />
     <QuizQuestion />
 </template>
 
