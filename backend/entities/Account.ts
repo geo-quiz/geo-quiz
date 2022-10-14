67d2e9a2699ad6ae0ecbe78982dd49ef42f66f68
ToniKaru <toni.karu@gmail.com>
2022-10-14 13:15:01 +0200

    Correct capitalization of Account

--- backend/entities/Account.ts
+++ backend/entities/Account.ts
@@ -1,7 +1,7 @@
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
-export class account {
+export class Account {
     constructor(email: string, passwordHash: string) {
         this.email = email;
         this.passwordHash = passwordHash;
