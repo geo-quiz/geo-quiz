135900c9bd0f1cea2446b5fc3552412655c220c3
Marcus Leeman <marcus.leeman@iths.se>
2022-10-19 15:03:39 +0200

    Add view: ProfileView

---
+++ frontend/src/views/ProfileView.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import MainMenuContainer from '@/components/MainMenuContainer.vue';
+import PageHeader from '@/components/PageHeader.vue';
+import ProfileEditor from '@/components/ProfileEditor.vue';
+import PageFooter from '@/components/PageFooter.vue';</script>
+
+<template>
+    <MainMenuContainer noLogo>
+        <PageHeader />
+        <ProfileEditor />
+        <PageFooter />
+    </MainMenuContainer>
+</template>
+
+<style scoped>
+</style>
