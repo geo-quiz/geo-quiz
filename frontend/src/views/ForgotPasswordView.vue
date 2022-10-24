c10e09d1d1ca0576f251ddb6b2f1175ef0af2fba
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-10-24 11:50:18 +0200

    Add: ForgotPasswordView.vue

---
+++ frontend/src/views/ForgotPasswordView.vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import PageHeader from '@/components/PageHeader.vue';
+import ForgotPassword from '@/components/ForgotPassword.vue';
+import PageFooter from '@/components/PageFooter.vue';</script>
+
+<template>
+    <div class="center-vertical">
+        <PageHeader />
+        <ForgotPassword />
+        <PageFooter />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
