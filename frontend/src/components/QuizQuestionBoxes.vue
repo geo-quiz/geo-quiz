09a0bf75d12e0bff69ffb82c5f0343da53378c0e
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-10-24 16:36:48 +0200

    Add component: QuizQuestionBoxes

---
+++ frontend/src/components/QuizQuestionBoxes.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+import QuizQuestionBox from '@/components/QuizQuestionBox.vue';
+
+const props = defineProps({
+    nrOfQuestions: {
+        type: Number,
+        required: true,
+    }
+});
+</script>
+
+<template>
+    <div class="box-wrapper">
+        <QuizQuestionBox v-for="(index) in props.nrOfQuestions" :key="index" :index="index" />
+    </div>
+</template>
+
+<style scoped>
+.box-wrapper {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    gap: var(--gap);
+}
+</style>
