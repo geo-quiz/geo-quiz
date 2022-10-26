3fd93a15cb4ed57014811d148b41d0f2f6934e31
OscarStjernfeldt <oscar.eriksson.stjernfeldt@iths.se>
2022-10-26 09:20:51 +0200

    Remove center-vertical div

--- frontend/src/views/StartView.vue
+++ frontend/src/views/StartView.vue
@@ -5,17 +5,15 @@ import GeoButton from '@/components/GeoButton.vue';
 import router from '@/router/index';</script>
 
 <template>
-    <div class="center-vertical">
-        <MainMenuContainer>
-            <div class="buttonContainer">
-                <GeoButton size="large" color="green" id="login-button" height="75px" width="400px"
-                           @click="router.push('/login')">Sign in
-                </GeoButton>
-                <GeoButton height="75px" width="400px" @click="router.push('/register')">Register</GeoButton>
-            </div>
-            <PageFooter />
-        </MainMenuContainer>
-    </div>
+    <MainMenuContainer>
+        <div class="buttonContainer">
+            <GeoButton id="login-button" color="green" height="75px" size="large" width="400px"
+                       @click="router.push('/login')">Sign in
+            </GeoButton>
+            <GeoButton height="75px" width="400px" @click="router.push('/register')">Register</GeoButton>
+        </div>
+        <PageFooter />
+    </MainMenuContainer>
 </template>
 
 <style scoped>
