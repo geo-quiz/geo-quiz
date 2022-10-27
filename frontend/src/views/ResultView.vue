fd6dac9e2fbedf3be6ae5c4ccbcc999b8cae6a76
ChristianLofqvist <christian.lofqvist@iths.se>
2022-10-27 15:23:53 +0200

    Add ResultView

---
+++ frontend/src/views/ResultView.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup>
+import PageHeader from '@/components/PageHeader.vue';
+import PageResult from '@/components/PageResult.vue';
+import PageFooter from '@/components/PageFooter.vue';</script>
+
+<template>
+    <PageHeader />
+    <PageResult />
+    <PageFooter />
+</template>
+
+<style scoped></style>
