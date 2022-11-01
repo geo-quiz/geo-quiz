d942d60b19455721b6da25519cf07a68f533f379
ToniKaru <toni.karu@gmail.com>
2022-11-01 11:22:41 +0100

    Add basic LeaderboardView.vue

---
+++ frontend/src/views/LeaderboardView.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+import PageHeader from '@/components/PageHeader.vue';
+import LeaderboardTable from '@/components/LeaderboardTable.vue';
+</script>
+
+<template>
+    <PageHeader />
+    <LeaderboardTable />
+</template>
+
+<style scoped></style>
