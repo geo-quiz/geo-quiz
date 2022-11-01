f187810ac462f31a784ab54fd73e0150a45bca6b
Marcus Leeman <marcus.leeman@iths.se>
2022-11-01 15:36:54 +0100

    Put all components in the same view

--- frontend/src/views/ContinentViewMobile.vue
+++ frontend/src/views/ContinentViewMobile.vue
@@ -1,10 +1,16 @@
 <script lang="ts" setup>
 import ContinentMapMobile from '@/components/ContinentMapMobile.vue';
-import PageHeader from '@/components/PageHeader.vue';</script>
+import ContinentMapMobileLeft from '@/components/ContinentMapMobileLeft.vue';
+import ContinentMapMobileRight from '@/components/ContinentMapMobileRight.vue';
+import { ref } from 'vue';
+
+const page = ref('middle');
+</script>
 
 <template>
-  <PageHeader />
-  <ContinentMapMobile />
+    <ContinentMapMobileLeft v-if="page === 'left'" @goTo="(v) => (page = v)" />
+    <ContinentMapMobileRight v-else-if="page === 'right'" @goTo="(v) => (page = v)" />
+    <ContinentMapMobile v-else @goTo="(v) => (page = v)" />
 </template>
 
 <style scoped></style>
