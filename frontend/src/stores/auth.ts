85c577c81db2fc342fc0407dbd1c5b5bf8e0ca3e
OscarStjernfeldt <oscarstjernfeldt@hotmail.com>
2022-11-02 10:57:44 +0100

    Add Loader to logout function

--- frontend/src/stores/auth.ts
+++ frontend/src/stores/auth.ts
@@ -1,50 +1,51 @@
 import { defineStore } from 'pinia';
 import router from '@/router';
-import { ref } from 'vue';
 import type { Ref } from 'vue';
+import { ref } from 'vue';
 
 export const useAuthStore = defineStore('auth', () => {
     const token: Ref<string | null> = ref('');
-    const awaitingResponse = ref(false);
 
     function setToken(newToken: string) {
         token.value = newToken;
     }
 
-    function logout() {
-        let potentialToken = localStorage.getItem('token');
-        if (potentialToken) {
-            token.value = potentialToken;
-            console.log('Local ', token.value);
-        } else {
-            potentialToken = sessionStorage.getItem('token.value');
+    function logout(): Promise<Boolean> {
+        return new Promise<Boolean>((success, fail) => {
+            let potentialToken = localStorage.getItem('token');
             if (potentialToken) {
                 token.value = potentialToken;
-                console.log('Session ', token.value);
-            }
-        }
-        if (token.value) {
-            awaitingResponse.value = true;
-            fetch('http://localhost:3000/logout', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({ token: token.value }),
-            }).then((res) => {
-                awaitingResponse.value = false;
-                if (res.ok) {
-                    sessionStorage.removeItem('token');
-                    localStorage.removeItem('token');
-                    token.value = null;
-                    router.push('/');
-                } else {
-                    alert('Something went wrong');
+            } else {
+                potentialToken = sessionStorage.getItem('token.value');
+                if (potentialToken) {
+                    token.value = potentialToken;
                 }
-            });
-        } else {
-            alert('Something went wrong');
-        }
+            }
+            if (token.value) {
+                fetch('http://localhost:3000/logout', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ token: token.value }),
+                }).then((res) => {
+                    if (res.ok) {
+                        sessionStorage.removeItem('token');
+                        localStorage.removeItem('token');
+                        token.value = null;
+                        router.push('/');
+                        success(true);
+                    } else {
+                        fail('Response not OK');
+                    }
+                }).catch((error) => {
+                    console.error(error);
+                    fail('Failed to fetch');
+                });
+            } else {
+                fail('No token');
+            }
+        });
     }
 
     return { token, logout, setToken };
